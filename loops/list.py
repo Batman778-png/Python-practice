6625bb8100fed3edc8a45f7aad32cdc9de940094
Batman778-png <deveshmokashi12@gmail.com>
2026-01-11 23:03:31 +0530

    Create list.py

---
+++ loops/list.py
@@ -0,0 +1,23 @@
+#list
+names=["Krish","Jack","Jacob",1,2,3,4,5]
+print(names)
+o/p:['Krish', 'Jack', 'Jacob', 1, 2, 3, 4, 5]
+
+
+### Accessing List Elements
+fruits=["apple","banana","cherry","kiwi","gauva"]
+print(fruits[0])
+print(fruits[2])
+print(fruits[4])
+print(fruits[-1])
+o/p:apple
+   cherry
+   gauva
+   gauva  
+
+
+print(fruits[1:])
+print(fruits[1:3])
+o/p:
+['banana', 'cherry', 'kiwi', 'gauva']
+['banana', 'cherry']
