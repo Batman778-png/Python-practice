c9a05634ca91a83cc0ebb3b688392c2df61b08f6
Batman778-png <deveshmokashi12@gmail.com>
2026-01-15 15:16:04 +0530

    Update tuple.py

--- loops/tuple.py
+++ loops/tuple.py
@@ -46,7 +46,67 @@ while i<len(fruits):
     print(fruits[i])
     i+=1
 
+#tuple methods
+#tuples have 2 method
+color=('blue','green','orange','blue')
 
+#count
+print(color.count('blue'))  #o/p:2
+
+#index
+color=('blue','green','orange','blue')
+print(color.index('green'))  #o/p:1
+
+#tuple functions
+num=(2,3,4,1)
+#len
+print(len(num)) #o/p:4
+
+#tuple functions
+num=(2,3,4,1,5,7)
+
+#sum
+print(sum(num)) #o/p:22
+#min and max
+print(min(num))#o/p:1
+print(max(num))#o/p:7
+
+
+#sort
+num=(7,4,3,1,5,2)
+a=sorted(num)
+num_sorted=tuple(a)
+print(num_sorted) #o/p:[1, 2, 3, 4, 5, 7]
+
+#TUPLE PACK AND UNPACK
+a="Madhav"
+b=21
+c="Engineer"# ye kya krta jo multiple variables he vo pack ke andar
+
+tuple_pack=a,b,c 
+print(tuple_pack)
+
+#TUPLE PACK AND UNPACK
+a="Madhav"
+b=21
+c="Engineer"
+tuple_pack=a,b,c
+name,age,profession=tuple_pack
+print(name)
+print(age)
+print(profession)
+
+#modifying tuple
+tuple_num=(10,20,30)
+#tuple_num[0] = 100 #error
+
+#how to mutate/modify tuple
+list_num=list(tuple_num)
+print(list_num) #o/p:[10, 20, 30]
+
+
+list_num[0]=100
+print(list_num)#o/p:[100, 20, 30]
 
 
 
